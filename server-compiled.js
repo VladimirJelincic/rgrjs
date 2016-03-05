cd98baf66c0da7385fa2386308a604f0ed32a000
Vladimir Jelincic <vladimir.jelincic@gmail.com>
2016-03-05 10:55:29 +0100

    Flux stores and actions added, mongo

--- server-compiled.js
+++ server-compiled.js
@@ -16,8 +16,7 @@ var app = (0, _express2.default)();
 app.use(_express2.default.static('public'));
 
 var db = undefined;
-var mongoUrl = "mongodb://rgrjs:asmodeus@ds019698.mlab.com:19698/rgrjs_vj";
-_mongodb.MongoClient.connect(mongoUrl, function (err, database) {
+_mongodb.MongoClient.connect(process.env.MONGO_URL, function (err, database) {
     if (err) throw err;
     db = database;
 
@@ -25,7 +24,8 @@ _mongodb.MongoClient.connect(mongoUrl, function (err, database) {
         return console.log('Listening on port 3000');
     });
 });
-app.get('/data/links', function (req, res) {
+
+app.get("/data/links", function (req, res) {
     db.collection("links").find({}).toArray(function (err, links) {
         if (err) throw err;
         res.json(links);
--- server.js
+++ server.js
@@ -7,17 +7,17 @@ let app=express();
 app.use(express.static('public'));
 
 let db;
-let mongoUrl="mongodb://rgrjs:asmodeus@ds019698.mlab.com:19698/rgrjs_vj"
-MongoClient.connect(mongoUrl,(err,database)=>{
+MongoClient.connect(process.env.MONGO_URL, (err, database) => {
     if(err) throw err;
     db=database;
 
     app.listen(3000,()=>console.log('Listening on port 3000'));
 
 });
-app.get('/data/links',(req,res)=>{
+
+app.get("/data/links", (req, res) => {
     db.collection("links").find({}).toArray((err,links)=>{
         if(err) throw err;
         res.json(links);
-    })
-})
+  });
+});
