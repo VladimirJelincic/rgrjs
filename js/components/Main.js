69cad385edc6d73ff53c038ba9509b68851677de
Vladimir Jelincic <vladimir.jelincic@gmail.com>
2016-03-05 10:40:03 +0100

    Flux stores and actions added

--- js/components/Main.js
+++ js/components/Main.js
@@ -5,36 +5,40 @@ import React from "react";
 import API from "../API";
 import LinkStore from "../stores/LinkStore";
 
-let _getAppState=()=>{
+let _getAppState = ()=> {
     return {links: LinkStore.getAll()};
 }
 
 export default class Main extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state=_getAppState();
-        this.onChange=this.onChange.bind(this);
+        this.state = _getAppState();
+        this.onChange = this.onChange.bind(this);
     }
-    componentWillMount(){
-       // debugger;
+
+    componentWillMount() {
+        // debugger;
     }
 
     componentDidMount() {
         API.fetchLinks();
-        LinkStore.on("change",this.onChange)
+        LinkStore.on("change", this.onChange)
     }
-    componentWillUnmount(){
-        LinkStore.removeListener("change",this.onChange);
+
+    componentWillUnmount() {
+        LinkStore.removeListener("change", this.onChange);
     }
-    onChange(){
+
+    onChange() {
         console.log("4. In the View");
         this.setState(_getAppState);
     }
+
     render() {
-        let content=this.state.links.map(link=>{
+        let content = this.state.links.map(link=> {
             return <li key={link._id}>
                 <a href={link.url}>{link.title}</a>
-                </li>
+            </li>
 
         })
         return (
